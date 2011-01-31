12b0aaedeb7defe67c355e7c65737f5f7fa1d46b
mfields <michael@mfields.org>
2011-01-31 01:07:16 -0800

    Update todo list.

--- functions.php
+++ functions.php
@@ -26,12 +26,9 @@
  * @todo Add donation link in the settings page.
  * @todo Pretty-up the calendar widget.
  * @todo Alpha for yellow-dots.png. Change name to halftone-small.png.
- * @todo Ensure that all settings actually do something.
- * @todo Less saturation on #author-box bg color.
  * @todo Make dialog colors match new theme colors.
  * @todo Rename .heading-action class everywhere.
  * 
- * @todo *Desaturate all text that appears outside of #page.
  * @todo Left margin on blockquotes.
  * @todo Bottom margin on quote post h2's.
  * 
