cec85a7ad867273c3836163ee9008ba5f2026e42
mfields <michael@mfields.org>
2011-03-27 08:59:30 -0800

    Introducing basic versions of header and footer templates.

---
+++ footer-basic.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Alternative Footer Template.
+ *
+ * Just like footer.php with the following things omitted:
+ * 
+ * Paged navigation.
+ * Widgetized areas.
+ * Secondary theme location for nav menu.
+ *
+ * This file should close any html tags opened in header-no-nav.php.
+ * 
+ * @package      Ghostbird
+ * @author       Michael Fields <michael@mfields.org>
+ * @copyright    Copyright (c) 2011, Michael Fields
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since        1.1
+ */
+?>
+
+</div><!-- #page -->
+
+<div id="page-footer"></div>
+
+</div><!-- wrap -->
+
+<?php wp_footer(); ?>
+<!--[if IE 8]>
+<script>
+var imgs, i, w;
+var imgs = document.getElementsByTagName( 'img' );
+for( i = 0; i < imgs.length; i++ ) {
+	w = imgs[i].getAttribute( 'width' );
+	if ( 615 < w ) {
+		imgs[i].removeAttribute( 'width' );
+		imgs[i].removeAttribute( 'height' );
+	}
+}
+</script>
+<![endif]-->
+</body>
+</html>
---
+++ header-basic.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Alternative Header Template.
+ *
+ * Just like header.php only without the nav menu.
+ * 
+ * @package      Ghostbird
+ * @author       Michael Fields <michael@mfields.org>
+ * @copyright    Copyright (c) 2011, Michael Fields
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since        1.1
+ */
+?><!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<title><?php bloginfo( 'blogname' ); ?><?php wp_title(); ?></title>
+<link rel="stylesheet" media="all" href="<?php print get_stylesheet_uri(); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div id="wrap">
+	<div id="header">
+		<?php do_action( 'ghostbird_logo',       '<div id="logo">',       '</div>' ); ?>
+		<?php do_action( 'ghostbird_site_title', '<div id="site-title">', '</div>' ); ?>
+		<?php do_action( 'ghostbird_tagline',    '<div id="tagline">',    '</div>' ); ?>
+	</div>
+	
+	<div id="menu-top"></div>
+	
+	<div id="page" role="document">
