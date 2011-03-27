9efde693ddc5346ddb4c89b065f3fe81b39d6a02
mfields <michael@mfields.org>
2011-03-27 09:00:09 -0800

    Introducing template for longdesc plugin.

---
+++ longdesc-template.php
@@ -0,0 +1,21 @@
+<?php
+	get_header( 'basic' );
+
+	print '<div id="content">';
+	print '<div class="entry">';
+
+	the_content();
+
+	if( isset( $_GET['referrer'] ) && function_exists( 'longdesc_return_anchor' ) ) {
+		$uri = get_permalink( (int) $_GET['referrer'] );
+		if( !empty( $uri ) ) {
+			$uri.= '#' . longdesc_return_anchor( $id );
+			print '<p><a class="button" href="' . esc_url( $uri ) . '">' . esc_html__( 'Return to article.', 'ghostbird' ) . '</a></p>';
+		}
+	}
+
+	print '</div>';
+	print '</div>';
+
+	get_footer( 'basic' );
+?>
