dddaf2ef1329d42f1c31d409f934211c0d93c5fe
mfields <michael@mfields.org>
2011-04-01 04:23:47 -0800

    Need to print description here.

--- tag.php
+++ tag.php
@@ -24,7 +24,7 @@ get_header( 'tag' );
 
 	<div id="intro">
 		<h1><?php single_tag_title(); ?></h1>
-		<div id="summary"><?php tag_description(); ?></div>
+		<div id="summary"><?php print tag_description(); ?></div>
 		<div id="intro-meta">
 			<?php printf( _n( '%1$s entry has been tagged with the term &#8220;%2$s&#8221.', '%1$s entries have been tagged with the term &#8220;%2$s&#8221.', (int) $wp_query->found_posts, 'ghostbird' ), number_format_i18n( (int) $wp_query->found_posts ), single_tag_title( '', false ) ); ?>
 			<span class="subscribe"> <a href="<?php print esc_url( get_tag_feed_link( $wp_query->get_queried_object_id() ) ) ?>" title="<?php printf( esc_attr__( 'Get updated whenever a new entry is tagged with the term &#8220;%1$s&#8221;.', 'ghostbird' ), single_tag_title( '', false ) ); ?>"><?php esc_html_e( 'Subscribe', 'ghostbird' ) ?></a></span>
