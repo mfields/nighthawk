4df9696c7e756acc39d0bf1fc1958e1bc4a6f4b4
mfields <michael@mfields.org>
2011-04-02 14:25:46 -0800

    Use loop-post.php + add dynamic context to template parts.

--- single.php
+++ single.php
@@ -16,53 +16,15 @@ if ( ! have_posts() ) {
 	get_template_part( '404' );
 }
 
-get_header();
+$ghostbird_post_type = get_post_type();
+
+get_header( $ghostbird_post_type );
 
 ?>
 
 <div id="content">
 
-<?php
-while ( have_posts() ) {
-	the_post();
-?>
-
-	<div id="intro">
-		<?php the_title( '<h1>', '</h1>' ); ?>
-		<?php
-			$author_name = get_the_author();
-			if ( ! empty( $author_name ) ) {
-				print '<p id="byline">' . sprintf( esc_html__( 'By %1$s', 'ghostbird' ), $author_name ) . '</p>';
-			}
-		?>
-		<?php ghostbird_summary( '<div id="summary">', '</div>' ); ?>
-		<?php ghostbird_summary_meta( '<div id="intro-meta">', '</div>' ); ?>
-	</div>
-
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<?php do_action( 'ghostbird_entry_start' ); ?>
-
-	<?php ghostbird_featured_image( '<div class="featured-image">', '</div>' ); ?>
-
-	<div class="entry-content">
-	<?php the_content(); ?>
-	</div><!--entry-content-->
-
-	<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'ghostbird' ), 'after' => '</div>' ) ); ?>
-
-	<div class="entry-meta">
-	<?php ghostbird_entry_meta_date(); ?>
-	<?php ghostbird_entry_meta_taxonomy(); ?>
-	</div><!--meta-->
-
-	<?php do_action( 'ghostbird_entry_end' ); ?>
-
-	</div><!--entry-->
-
-<?php
-}
-?>
+<?php get_template_part( 'loop', $ghostbird_post_type ) ?>
 
 <?php ghostbird_author_bio(); ?>
 
@@ -72,4 +34,4 @@ while ( have_posts() ) {
 
 <div class="clear"></div>
 
-<?php get_footer(); ?>
+<?php get_footer( $ghostbird_post_type ); ?>
