780501df1ffa8aa4131cabcd59baf7171638ffbc
mfields <michael@mfields.org>
2011-04-03 08:02:25 -0700

    loop.php should be a fallback for custom post_types. Reflect this in code as well as docs.

--- loop.php
+++ loop.php
@@ -1,67 +1,51 @@
 <?php
 /**
- * Loop template
+ * The Default Loop for Archives.
  *
- * This file is responsible for generating all code in 
- * the WordPress loop.
+ * Generate The Loop for any post_type.
+ * This file is basically a fallback for
+ * custom post_types that do not have a
+ * dedicated loop file. This file will
+ * attempt to display as much info as
+ * possible for the post_type.
  *
  * @package      Ghostbird
  * @author       Michael Fields <michael@mfields.org>
  * @copyright    Copyright (c) 2011, Michael Fields
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since        1.0
+ * @alter        1.1
  */
 
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
+		$ghostbird_post_type = get_post_type();
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+<?php do_action( 'ghostbird_entry_start' ); ?>
+
 <?php
 
-	do_action( 'ghostbird_entry_start' );
+	/* Title. */
+	if ( post_type_supports( $ghostbird_post_type, 'title' ) ) {
+		the_title( "\n" . '<h1>', '</h1>' );
+	}
 
-	switch ( get_post_format() ) {
-		case 'aside' :
-		case 'link' :
-			print "\n" . '<div class="entry-content">';
-			the_content( __( 'Continue Reading', 'ghostbird' ) );
-			print "\n" . '</div><!--entry-content-->';
-			break;
-		case 'status' :
-			ghostbird_featured_image( '<div class="featured-image">', '</div>' );
-			print "\n" . '<div class="entry-content">';
-			the_content( __( 'Continue Reading', 'ghostbird' ) );
-			print "\n" . '</div><!--entry-content-->';
-			break;
-		default :
-			ghostbird_featured_image( '<div class="featured-image">', '</div>' );
-			/*
-			 * Title only for multiple views.
-			 * Will be displayed in single views via ghostbird_title() in an H1 element.
-			 */
-			if ( ! is_singular() ) {
-				the_title( "\n" . '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );
-			}
-
-			print "\n" . '<div class="entry-content">';
-			if ( ( is_archive() || is_home() ) && ( 'page' == get_post_type() || 'gallery' == get_post_format() ) ) {
-				the_excerpt();
-			}
-			else {
-				the_content( __( 'Continue Reading', 'ghostbird' ) );
-			}
-			print "\n" . '</div><!--entry-content-->';
-
-			wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'ghostbird' ), 'after' => '</div>' ) );	
-
-			print '<div class="' . esc_attr( ghostbird_entry_meta_classes() ) . '">';
-			ghostbird_entry_meta_date();
-			ghostbird_entry_meta_taxonomy();
-			print '</div><!--meta-->';
-			break;
+	/* Content. */
+	if ( post_type_supports( $ghostbird_post_type, 'editor' ) ) {
+		print "\n" . '<div class="entry-content">';
+		the_content( __( 'Continue Reading', 'ghostbird' ) );
+		print "\n" . '</div><!--entry-content-->';
+		wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'ghostbird' ), 'after' => '</div>' ) );
+	}
+	/* Use excerpt if post_type supports it instead of content. */
+	else if ( post_type_supports( $ghostbird_post_type, 'excerpt' ) ) {
+		print "\n" . '<div class="entry-content">';
+		the_excerpt();
+		print "\n" . '</div><!--entry-content-->';
 	}
 
 	do_action( 'ghostbird_entry_end' );
@@ -72,4 +56,3 @@ if ( have_posts() ) {
 <?php
 	}
 }
-?>
