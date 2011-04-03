1fb32221625d1d2d59f4bf73065ef75035466b6c
mfields <michael@mfields.org>
2011-04-03 07:07:35 -0700

    Move the loop back into single.php + use biography template part.

--- single.php
+++ single.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * Post Template.
+ * Default Single Post Template.
  *
- * This template is loaded whenever a single post is being viewed.
+ * This template is loaded whenever a single
+ * post is being viewed. This template has been
+ * coded to handle any public post_type.
  *
  * @package      Ghostbird
  * @author       Michael Fields <michael@mfields.org>
@@ -24,9 +26,53 @@ get_header( $ghostbird_post_type );
 
 <div id="content">
 
-<?php get_template_part( 'loop', $ghostbird_post_type ) ?>
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+?>
 
-<?php ghostbird_author_bio(); ?>
+<div id="intro">
+<?php
+	/* Title. */
+	if ( post_type_supports( $ghostbird_post_type, 'title' ) ) {
+		the_title( "\n" . '<h1>', '</h1>' );
+	}
+
+	/* Byline. */
+	if ( post_type_supports( $ghostbird_post_type, 'author' ) ) {
+		print "\n" . '<p id="byline">' . sprintf( esc_html__( 'By %1$s', 'ghostbird' ), get_the_author() ) . '</p>';
+	}
+?>
+</div>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php do_action( 'ghostbird_entry_start' ); ?>
+
+	<?php ghostbird_featured_image( '<div class="featured-image">', '</div>' ); ?>
+
+	<div class="entry-content">
+	<?php the_content(); ?>
+	</div><!--entry-content-->
+
+	<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'ghostbird' ), 'after' => '</div>' ) ); ?>
+
+	<div class="<?php print esc_attr( ghostbird_entry_meta_classes() ); ?>">
+		<?php ghostbird_entry_meta_date(); ?>
+		<?php ghostbird_entry_meta_taxonomy(); ?>
+	</div><!--meta-->
+
+	<?php do_action( 'ghostbird_entry_end' ); ?>
+
+</div><!--entry-->
+
+<?php
+	}
+}
+?>
+
+<?php get_template_part( 'biography', $ghostbird_post_type ); ?>
 
 <?php comments_template( '', true ); ?>
 
