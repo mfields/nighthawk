ca97bfdc816825977dbcf2429f63b561f8d9192c
mfields <michael@mfields.org>
2011-04-03 07:43:49 -0700

    Move The Loop back into page.php.

--- page.php
+++ page.php
@@ -21,7 +21,37 @@ get_header( 'page' );
 
 <div id="content">
 
-<?php get_template_part( 'loop', 'page' ); ?>
+<?php
+while ( have_posts() ) {
+	the_post();
+?>
+
+	<div id="intro">
+		<?php the_title( '<h1>', '</h1>' ); ?>
+		<p id="byline"><?php printf( esc_html__( 'By %1$s', 'ghostbird' ), get_the_author() ); ?></p>
+		<?php if ( has_excerpt() ) { ?>
+			<div id="summary"><?php the_excerpt() ?></div>
+		<?php } ?>
+		<?php ghostbird_summary_meta( '<div id="intro-meta">', '</div>' ); ?>
+	</div>
+
+	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php do_action( 'ghostbird_entry_start' ); ?>
+
+	<div class="entry-content">
+	<?php the_content(); ?>
+	</div><!--entry-content-->
+
+	<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'ghostbird' ), 'after' => '</div>' ) ); ?>
+
+	<?php do_action( 'ghostbird_entry_end' ); ?>
+
+	</div><!--entry-->
+
+<?php
+}
+?>
 
 <?php get_template_part( 'biography', 'page' ); ?>
 
