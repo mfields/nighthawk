cfc74f457273e672ca0a32f803294c66e5c66578
Michael Fields <michael@mfields.org>
2011-05-01 08:12:18 -0700

    Move loop inside until I figure out what to do with post formats.

--- single-post.php
+++ single-post.php
@@ -53,7 +53,45 @@ if ( ! empty( $nighthawk_post_format ) ) {
 
 <div id="content" class="contain">
 
-<?php get_template_part( 'loop', $nighthawk_context ); ?>
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+?>
+
+<div id="intro">
+	<?php nighthawk_featured_image( '<div id="featured-image">', '</div>' ); ?>
+	<?php the_title( '<h1>', '</h1>' ); ?>
+	<p id="byline"><?php printf( esc_html__( 'By %1$s', 'nighthawk' ), get_the_author() ); ?></p>
+	<?php nighthawk_summary_meta( '<div id="intro-meta">', '</div>' ); ?>
+</div>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class( 'clear' ); ?>>
+
+<?php
+
+	do_action( 'nighthawk_entry_start' );
+
+	print "\n" . '<div class="entry-content">';
+	the_content();
+	print "\n" . '</div><!--entry-content-->';
+
+	wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'nighthawk' ), 'after' => '</div>' ) );
+
+	print '<div class="' . esc_attr( nighthawk_entry_meta_classes() ) . '">';
+	nighthawk_entry_meta_date();
+	nighthawk_entry_meta_taxonomy();
+	print '</div><!--meta-->';
+
+	do_action( 'nighthawk_entry_end' );
+?>
+
+</div><!--entry-->
+
+<?php
+	}
+}
+?>
 
 <?php get_template_part( 'biography', $nighthawk_context ); ?>
 
