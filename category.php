f44cecc4166da2339636a7e22cd95827de78f770
Michael Fields <michael@mfields.org>
2011-05-03 06:25:03 -0700

    actually print the category description.

--- category.php
+++ category.php
@@ -21,7 +21,7 @@ get_header( 'category' );
 
 <div id="intro">
 	<h1><?php single_cat_title(); ?></h1>
-	<div id="summary"><?php category_description(); ?></div>
+	<div id="summary"><?php print category_description(); ?></div>
 	<div id="intro-meta">
 		<?php printf( _n( 'There is %1$s entry in this category.', 'There are %1$s entries in this category.', (int) $wp_query->found_posts, 'nighthawk' ), number_format_i18n( (int) $wp_query->found_posts ) ); ?>
 		<span class="subscribe"> <a href="<?php print esc_url( get_category_feed_link( $wp_query->get_queried_object_id() ) ) ?>" title="<?php printf( esc_attr__( 'Get updated whenever a new entry is added to the %1$s category.', 'nighthawk' ), single_cat_title( '', false ) ); ?>"><?php esc_html_e( 'Subscribe', 'nighthawk' ) ?></a></span>
