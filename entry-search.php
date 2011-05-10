f133ef5348d8639d1f347d766f86aef6a8ee3dd0
Michael Fields <michael@mfields.org>
2011-05-10 03:26:33 -0700

    Clean up whitespace in html.

--- entry-search.php
+++ entry-search.php
@@ -10,26 +10,29 @@
  */
 ?>
 
-<article id="<?php nighthawk_entry_id(); ?>" <?php post_class(); ?>>
 
+
+<article id="<?php nighthawk_entry_id(); ?>" <?php post_class(); ?>>
 <?php
 	do_action( 'nighthawk_entry_start' );
 
-	the_title( '<h2 class="entry-title heading">', '</h2>' );
+	the_title( "\t" . '<h2 class="entry-title heading">', '</h2>' );
 
-	print "\n" . '<p class="content">';
+	print "\n\t" . '<p class="content">';
 
-	print '<time class="date" pubdate="pubdate" datetime="' . esc_attr( get_post_time( 'Y-m-d\TH:i:s\Z', true ) ) . '">' . esc_html( get_the_time( 'M j, Y' ) ) . '</time> &#8211; ';
+	print "\n\t\t" . '<time class="date" pubdate="pubdate" datetime="' . esc_attr( get_post_time( 'Y-m-d\TH:i:s\Z', true ) ) . '">' . esc_html( get_the_time( 'M j, Y' ) ) . '</time> &#8211; ';
 
+	print "\n\t\t" . '<span class="post-excerpt">';
 	the_excerpt();
+	print '</span>';
 
-	print ' <a class="permalink" href="' . esc_url( get_permalink() ) . '">' . sprintf( esc_html__( 'View this %1$s', 'ghostbird' ), nighthawk_post_label_singular() ) . '</a>';
+	print "\n\t\t" . '<a class="permalink" href="' . esc_url( get_permalink() ) . '">' . sprintf( esc_html__( 'View this %1$s', 'ghostbird' ), nighthawk_post_label_singular() ) . '</a>';
 
-	print "\n" . '</p><!--content-->';
+	print "\n\t" .'</p>';
 
 	do_action( 'nighthawk_entry_end' );
 ?>
 
-</article><!--entry-->
+</article>
 
 <?php do_action( 'nighthawk_append_to_entry_template' ); ?>
