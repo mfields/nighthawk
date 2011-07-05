23880dd0b02a3f2b97321d48bc522c97ade6ec2f
Michael Fields <michael@mfields.org>
2011-07-05 06:25:04 -0700

    Widget Dropdown updates.

--- dropdowns.js
+++ dropdowns.js
@@ -1,9 +1,11 @@
 jQuery( document ) .ready( function( $ ) {
 
 	var config = {
-		container : '#dropdown-widgets',
-		dropdowns : '.dropdown',
-		triggers  : '.widget-title'
+		container   : '#dropdown-widgets',
+		dropdowns   : '.dropdown',
+		triggers    : '.widget-title',
+	//	maxBoxWidth : 765
+		maxBoxWidth : 400
 	};
 
 	var widgets = $( config.container );
@@ -80,7 +82,7 @@ jQuery( document ) .ready( function( $ ) {
 		var boxPos = active.box.position();
 		var triggerPos = active.trigger.position();
 		var rightEdge = boxPos.left + active.box.outerWidth();
-
+		
 		if ( rightEdge > parent.innerWidth() ) {
 			active.box.css( {
 				left  : 'auto',
@@ -151,6 +153,11 @@ jQuery( document ) .ready( function( $ ) {
 			 */
 			box.css( css );
 			link.addClass( 'active' );
+
+			if ( box.hasClass( 'widget_search' ) ) {
+				box.find( '[name]=s' ).focus();
+			}
+
 
 			active = {
 				box     : box,
