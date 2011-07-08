1503e8acd69feb783af1ea7bb4d5e284de635abf
Michael Fields <michael@mfields.org>
2011-07-08 09:45:24 -0700

    Use dynamic post_type name & move meta information from functions.php

--- archive-mfields_bookmark.php
+++ archive-mfields_bookmark.php
@@ -11,17 +11,34 @@
  * @since        1.0
  */
 
+$nighthawk_post_type = 'mfields_bookmark';
+
 if ( ! have_posts() ) {
-	get_template_part( '404', 'archive-mfields_bookmark' );
+	get_template_part( '404', $nighthawk_post_type );
 }
 
-get_header( 'archive-mfields_bookmark' );
+get_header( $nighthawk_post_type );
 
 ?>
 
 <div id="intro">
+
 	<h1>Bookmarks</h1>
-	<?php nighthawk_summary_meta( '<div id="intro-meta">', '</div>' ) ?>
+
+	<div id="intro-meta"><?php
+
+		global $wp_query;
+		$total = 0;
+		if ( isset( $wp_query->found_posts ) ) {
+			$total = (int) $wp_query->found_posts;
+		}
+
+		printf( _n( 'There is %1$s bookmark in this section.', 'There are %1$s links in this section.', $total, 'nighthawk' ), number_format_i18n( $total ) );
+
+		print ' <span class="subscribe"><a href="' . esc_url( get_post_type_archive_feed_link( $nighthawk_post_type ) ) . '" title="' . esc_attr( __( 'Get updated whenever a new link is bookmarked.', 'nighthawk' ) ) . '">' . esc_html__( 'Subscribe', 'nighthawk' ) . '</a></span>';
+
+	?></div>
+
 </div>
 
 <div id="content" class="contain">
@@ -35,4 +52,4 @@ get_header( 'archive-mfields_bookmark' );
 
 <div id="page-footer"><?php do_action( 'nighthawk_paged_navigation' ); ?></div>
 
-<?php get_footer( 'archive-mfields_bookmark' ); ?>
+<?php get_footer( 'archive-' . $nighthawk_post_type ); ?>
