47618fa8d65982dac924af89ee2b72d70cbfd58c
Michael Fields <michael@mfields.org>
2011-08-22 14:01:05 -0700

    Archive page for status updates.

---
+++ taxonomy-post_format-post-format-status.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Blog timeline template
+ *
+ * This file is responsible for creating the blog view.
+ * In a default installation of WordPress, this will be
+ * the home page. In instances where users have designated
+ * a page to be used as their "Blog Page", this template
+ * will be used instead of page.php.
+ *
+ * @package      Nighthawk
+ * @author       Michael Fields <michael@mfields.org>
+ * @copyright    Copyright (c) 2011, Michael Fields
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since        1.0
+ */
+
+if ( ! have_posts() ) {
+	get_template_part( '404', 'post' );
+}
+
+get_header( 'post' );
+
+?>
+
+<header id="intro">
+	<h1><?php _e( 'Status Updates' ); ?></h1>
+	<?php nighthawk_summary_meta( '<div id="intro-meta">', '</div>' ) ?>
+</header>
+
+<div id="blog" class="contain">
+
+<?php
+while ( have_posts() ) {
+	the_post();
+	get_template_part( 'entry', 'post' );
+}
+?>
+
+</div><!--content-->
+
+<div id="page-footer"><?php do_action( 'nighthawk_paged_navigation' ); ?></div>
+
+<?php get_footer( 'post' ); ?>
