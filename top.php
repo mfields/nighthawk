1162b1eacfb1aff26e3c7d506f2fc28d73c671a6
Michael Fields <michael@mfields.org>
2012-02-02 00:51:16 -0800

    Decisions not options. Returning early was a bad choice here. Bad Mike, bad ...

--- top.php
+++ top.php
@@ -1,26 +1,18 @@
 <?php
 
-	if ( 0 != (int) get_theme_mod( 'nighthawk_display_site_title', 1 ) ) {
-		$text = get_bloginfo( 'blogname' );
-		if ( empty( $text ) ) {
-			return;
-		}
-		if ( is_front_page() && ! is_paged() ) {
+	$text = get_bloginfo( 'blogname' );
+	if ( ! empty( $text ) ) {
+		if ( is_front_page() && ! is_paged() )
 			$text = esc_html( $text );
-		}
-		else {
+		else
 			$text = '<a href="' . esc_url( home_url() ) . '">' . esc_html( $text ) . '</a>';
-		}
+
 		echo "\n" . '<div id="site-title">' . $text . '</div>';
 	}
 
-	if ( 0 != (int) get_theme_mod( 'nighthawk_display_tagline', 1 ) ) {
-		$text = get_bloginfo( 'description' );
-		if ( empty( $text ) ) {
-			return;
-		}
+	$text = get_bloginfo( 'description' );
+	if ( ! empty( $text ) )
 		echo "\n" . '<div id="tagline">' . esc_html( $text ) . '</div>';
-	}
 
 	wp_nav_menu( apply_filters( 'nighthawk_menu_args_primary', array(
 		'container'      => 'div',
